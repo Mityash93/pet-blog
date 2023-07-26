4813067d86e354fe941cbf14bd5bc35f6ca7ce10
Dmitriy Mityashkin <mit93dima@mail.ru>
2023-07-26 22:59:57 +0300

    feat: theme, classnames, gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Link, Route, Routes } from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.asyncLazy'
 import { MainPageAsync } from './pages/MainPage/MainPageAsync'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
+import { useTheme } from './theme/useTheme'
+import classNames from 'classnames'
+
+export enum Theme {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
+    <div
+      className={classNames('app', {
+        [theme]: true,
+      })}
+    >
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={'/about'} element={<AboutPageAsync />} />
