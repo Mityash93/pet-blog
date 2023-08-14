c1df9552e6bb9b131564d60aa3cb96c27921b643
Dmitriy Mityashkin <mit93dima@mail.ru>
2023-08-14 17:42:28 +0300

    feat: jest tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,12 +3,15 @@ import classNames from "classnames";
 import cls from "./Sidebar.module.scss";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { LangSwitcher } from "widgets/LangSwitcher";
+import { useTranslation } from "react-i18next";
+import { Button } from "shared/ui/Button/Button";
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const { t } = useTranslation();
   const [collapsed, setCollapsed] = useState(false);
 
   const onToggle = () => {
@@ -17,11 +20,14 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button onClick={onToggle}>toggle</button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        {t("toggle")}
+      </Button>
       <div className={cls.witchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
